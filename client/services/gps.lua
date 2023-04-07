1d6a0d187b7e9392ba9a13443649c926f3485b00
Jake2k4 <119366047+jakeyboi1@users.noreply.github.com>
2023-04-06 21:16:32 -0500

    Gps To Entity Added
    This adds a Gps To Entity function  to the gps api! This will place a Gps that will track an entity!
    To use:
    VORPutils.Gps:GpsToEntity(entity)
    
    To Remove it just use the already existing RemoveGps api
    VORPutils.Gps:RemoveGps()

--- client/services/gps.lua
+++ client/services/gps.lua
@@ -1,5 +1,7 @@
 GpsApi = {}
 
+local GpsToEntity = false
+
 function GpsApi:SetGps(x, y, z)
     local pl = GetEntityCoords(PlayerPedId())
     StartGpsMultiRoute(6, true, true)
@@ -8,6 +10,22 @@ function GpsApi:SetGps(x, y, z)
     SetGpsMultiRouteRender(true)
 end
 
+function GpsApi:GpsToEntity(entity)
+    GpsToEntity = true
+    local entityinitcoords = GetEntityCoords(entity)
+    GpsApi:SetGps(entityinitcoords.x, entityinitcoords.y, entityinitcoords.z)
+    while GpsToEntity do
+        Citizen.Wait(200)
+        local entitycoords = GetEntityCoords(entity)
+        if GetDistanceBetweenCoords(entityinitcoords.x, entityinitcoords.y, entityinitcoords.z, entitycoords.x, entitycoords.y, entitycoords.z, false) > 10 then
+            entityinitcoords = entitycoords
+            AddPointToGpsMultiRoute(entitycoords.x, entitycoords.y, entitycoords.z)
+            SetGpsMultiRouteRender(true)
+        end
+    end
+end
+
 function GpsApi:RemoveGps()
+    GpsToEntity = false
     ClearGpsMultiRoute()
 end
