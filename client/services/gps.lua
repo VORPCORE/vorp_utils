9c3204bc42137598f30ca587036378cb9f92dcf1
Outsider <87246847+outsider31000@users.noreply.github.com>
2023-08-01 18:14:01 +0100

    feat: add gps color
    as string or hash

--- client/services/gps.lua
+++ client/services/gps.lua
@@ -1,8 +1,18 @@
 GpsApi = {}
 
-function GpsApi:SetGps(x, y, z)
-    local pl = GetEntityCoords(PlayerPedId())
-    StartGpsMultiRoute(6, true, true)
+---@param x number
+---@param y number
+---@param z number
+---@param color string|number color of gps route
+function GpsApi:SetGps(x, y, z, color)
+    local player = PlayerPedId()
+    local pl = GetEntityCoords(player)
+
+    if color and type(color) == "string" then
+       color = joaat(color)
+    end
+
+    StartGpsMultiRoute(color or joaat("COLOR_RED"), true, true)
     AddPointToGpsMultiRoute(pl.x, pl.y, pl.z)
     AddPointToGpsMultiRoute(x, y, z)
     SetGpsMultiRouteRender(true)
