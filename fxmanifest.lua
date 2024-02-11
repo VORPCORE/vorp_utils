9ba575e59c22c9ddbbbfb3e1317099a5942e70ab
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-02-11 10:22:24 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,24 +1,26 @@
-game 'rdr3'
 fx_version 'adamant'
+game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+
 lua54 'yes'
 author "VORP @Bytesizd"
+description 'A library to help build your scripts for vorp core framework'
+repository 'https://github.com/VORPCORE/vorp_utils'
 
+shared_scripts {
+  'shared/helpers/*.lua',
+  'shared/services/*.lua'
+}
 client_scripts {
   'client/data/*.lua',
   'client/services/*.lua',
   'client/*.lua'
 }
-
 server_scripts {
   'server/main.lua',
   'server/helpers/*.lua',
   'server/services/*.lua'
-}
-
-shared_scripts {
-  'shared/helpers/*.lua',
-  'shared/services/*.lua'
 }
 
 version '1.1.1'
