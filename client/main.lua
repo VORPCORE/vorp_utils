d5b7f62bc4133b96ea9fd1c631f6de94aaf0d1f9
Celeste Capece <celedev97@gmail.com>
2025-01-24 15:10:11 +0100

    added an export to get utils
    this should allow to get the utils like this:
    local VORPutils = exports.vorp_utils:GetUtils()
    in a similar way to how vorp core is taken with exports instead of TriggerEvent

--- client/main.lua
+++ client/main.lua
@@ -1,18 +1,22 @@
+local UtilAPI = {}
+
+UtilAPI.General = GeneralAPI
+UtilAPI.Prompts = PromptsAPI
+UtilAPI.Blips = BlipAPI
+UtilAPI.Peds = PedAPI
+UtilAPI.DataView = DataView
+UtilAPI.Print = PrintAPI
+UtilAPI.Objects = ObjectAPI
+UtilAPI.Events = EventsAPI
+UtilAPI.Destruct = DestructionAPI
+UtilAPI.Render = RenderAPI
+UtilAPI.Gps = GpsApi
+UtilAPI.Shared = Shared
+
 AddEventHandler('getUtils', function(cb)
-    local UtilAPI = {}
-
-    UtilAPI.General = GeneralAPI
-    UtilAPI.Prompts = PromptsAPI
-    UtilAPI.Blips = BlipAPI
-    UtilAPI.Peds = PedAPI
-    UtilAPI.DataView = DataView
-    UtilAPI.Print = PrintAPI
-    UtilAPI.Objects = ObjectAPI
-    UtilAPI.Events = EventsAPI
-    UtilAPI.Destruct = DestructionAPI
-    UtilAPI.Render = RenderAPI
-    UtilAPI.Gps = GpsApi
-    UtilAPI.Shared = Shared
-
     cb(UtilAPI)
+end)
+
+exports('GetUtils', function()
+    return UtilAPI
 end)
